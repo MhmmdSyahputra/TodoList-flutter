e0a128bf3af8b02d119722efd707681d9ebea776
MhmmdSyahputra <putra.cokers@gmail.com>
2023-05-25 01:39:14 +0700

    add drawer and count todo by category in drawer

--- lib/views/MainMenuBottom.dart
+++ lib/views/MainMenuBottom.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:todo_sort/provider/providersTodos.dart';
 import 'package:todo_sort/views/MainTodo.dart';
 
 import '../GlobalFunction.dart';
@@ -31,6 +32,29 @@ class _BottomNavMainState extends State<BottomNavMain> {
     final provThemeMode = Provider.of<ThemeProvider>(context).enableDarkMode;
     final prov = Provider.of<ThemeProvider>(context);
 
+    final provCountTodo = Provider.of<TodoProvider>(context);
+
+    final totRoutine = provCountTodo.todoList
+        .where((allList) =>
+            allList.kategetori == "Routine" && allList.isCheck == false)
+        .map((allList) {})
+        .toList()
+        .length;
+
+    final totWork = provCountTodo.todoList
+        .where((allList) =>
+            allList.kategetori == "Work" && allList.isCheck == false)
+        .map((allList) {})
+        .toList()
+        .length;
+
+    final totOther = provCountTodo.todoList
+        .where((allList) =>
+            allList.kategetori == "Other" && allList.isCheck == false)
+        .map((allList) {})
+        .toList()
+        .length;
+
     Widget body = _widgetOptions.elementAt(_selectedIndex);
     switch (_selectedIndex) {
       case 0:
@@ -53,15 +77,6 @@ class _BottomNavMainState extends State<BottomNavMain> {
           backgroundColor: MyThemeHead(provThemeMode),
           actions: [
             provThemeMode ? Icon(Icons.dark_mode) : Icon(Icons.light_mode),
-            Switch(
-              value: provThemeMode,
-              activeColor: Colors.cyan,
-              onChanged: (bool value) {
-                setState(() {
-                  prov.SetBrightness(value);
-                });
-              },
-            )
           ],
         ),
         body: body,
@@ -97,6 +112,85 @@ class _BottomNavMainState extends State<BottomNavMain> {
               Colors.black, // ubah warna ikon yang dipilih menjadi hitam
           unselectedItemColor: Colors.black.withOpacity(
               0.6), // ubah warna ikon yang tidak dipilih menjadi hitam transparan
+        ),
+        drawer: Drawer(
+          backgroundColor: MyTheme(provThemeMode),
+          child: ListView(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  "Todo App",
+                  style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                      color: MyTheme(!provThemeMode)),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  "By Muhammad Syahputra",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: MyTheme(!provThemeMode)),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              ListTile(
+                title: Text(
+                  'Routine',
+                  style: TextStyle(color: MyTheme(!provThemeMode)),
+                ),
+                trailing: Visibility(
+                  visible: totRoutine != 0,
+                  child: CircleAvatar(
+                    child: Text(totRoutine.toString()),
+                  ),
+                ),
+              ),
+              ListTile(
+                title: Text(
+                  'Work',
+                  style: TextStyle(color: MyTheme(!provThemeMode)),
+                ),
+                trailing: Visibility(
+                  visible: totWork != 0,
+                  child: CircleAvatar(
+                    child: Text(totWork.toString()),
+                  ),
+                ),
+              ),
+              ListTile(
+                title: Text('Other',
+                    style: TextStyle(color: MyTheme(!provThemeMode))),
+                trailing: Visibility(
+                  visible: totOther != 0,
+                  child: CircleAvatar(
+                    child: Text(totOther.toString()),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              ListTile(
+                title: Text('Dark Mode',
+                    style: TextStyle(color: MyTheme(!provThemeMode))),
+                trailing: Switch(
+                  value: provThemeMode,
+                  activeColor: Colors.cyan,
+                  onChanged: (bool value) {
+                    setState(() {
+                      prov.SetBrightness(value);
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
