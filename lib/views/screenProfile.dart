995d56de8a7aad785bde0a3b16c68ef026420039
MhmmdSyahputra <putra.cokers@gmail.com>
2023-07-05 01:12:11 +0700

    progress bar

--- lib/views/screenProfile.dart
+++ lib/views/screenProfile.dart
@@ -22,6 +22,35 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Widget build(BuildContext context) {
     final provThemeMode = Provider.of<ThemeProvider>(context).enableDarkMode;
 
+    final provCountTodo = Provider.of<TodoProvider>(context);
+
+    listCheck() {
+      return provCountTodo.todoList
+          .where((allList) => allList.isCheck == true)
+          .map((allList) {})
+          .toList()
+          .length;
+    }
+
+    listUnCheck() {
+      return provCountTodo.todoList
+          .where((allList) => allList.isCheck == false)
+          .map((allList) {})
+          .toList()
+          .length;
+    }
+
+    allList() {
+      return provCountTodo.todoList.map((allList) {}).toList().length;
+    }
+
+    displayText() {
+      if (listUnCheck() == 0) {
+        return 'All Task done';
+      }
+      return 'You still have ${listUnCheck()} task(s) to do';
+    }
+
     return Container(
       color: myThemeBottom(provThemeMode),
       child: Padding(
@@ -37,6 +66,38 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   return CardCategory(data: res);
                 }).toList(),
               ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: Card(
+                  elevation: 4,
+                  color: myThemeBottom(provThemeMode),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(children: [
+                      SliderTheme(
+                        data: const SliderThemeData(
+                            thumbShape:
+                                RoundSliderThumbShape(disabledThumbRadius: 0),
+                            overlayShape:
+                                RoundSliderOverlayShape(overlayRadius: 0),
+                            disabledInactiveTrackColor: Colors.grey,
+                            disabledActiveTrackColor: Color(0xcc0077b6)),
+                        child: Slider(
+                            value: listCheck().toDouble(),
+                            max:
+                                allList().toDouble(), // Durasi lagu disesuaikan
+                            onChanged: null),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        '${displayText()}',
+                        style: TextStyle(color: myTheme(!provThemeMode)),
+                      )
+                    ]),
+                  )),
             )
           ],
         ),
